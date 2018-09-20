df053b3dc046958471d4267a05fc888a90d8ba1e
Hiromu Ishikawa <rock.lee.012086@gmail.com>
2018-09-20 10:16:09 +0900

    Add target category to all article

---
+++ extract_keyword_by_cated.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from backend.models.page import Article, Category
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import yaml
+import json
+import sys
+import collections
+
+if __name__ == "__main__":
+
+    raw = open("db/json/pro_cated_article_20180601.json", "r+")
+    articles_json = json.load(raw)
+    print(len(articles_json))
+    articles = [Article.from_json(json) for json in articles_json]
+
+    for a in articles:
+        print(a.title)
+        a.calc_tf()
+
+    out_data = []
+    for a in articles:
+        a.extract_key(len(articles))
+        js = a.to_json()
+        out_data.append(js)
+        print("{}: {}".format(a.title, a.keywords))
+
+    out = open("db/json/pro_keyed_article_20180601.json","w")
+    json.dump(out_data, out, ensure_ascii=False)
+
+    out.close()
+    raw.close()
