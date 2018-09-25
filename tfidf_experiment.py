30c065e707b1f9115cc23f2ff0a67433af37a870
Hiromu Ishikawa <rock.lee.012086@gmail.com>
2018-09-25 13:53:34 +0900

    Plot num of keywords

--- tfidf_experiment.py
+++ tfidf_experiment.py
@@ -24,17 +24,20 @@ def keywords(tfidf, alpha):
 def evaluate(articles, tfidf_d, alpha, hit):
     judge_l = []
     prec_l = np.empty(200)
+    all_keys = 0
 
     for i, a in enumerate(articles):
         tfidf = tfidf_d[tfidf_d["page_id"] == a.page_id].iloc[0].tiidf # caution: ファイル書き出し時点で tfidf -> tiidf のtypo 有
         keys = keywords(tfidf, alpha)
+        all_keys += len(keys)
         judge_l.append(judge(keys, hit))
         prec_l[i] = precision(keys, hit)
 
     rate = judge_l.count(True)
     mean_prec = np.mean(prec_l)
 
-    return {"alpha": alpha, "rate": rate, "mean_prec": mean_prec}
+    # return {"alpha": alpha, "rate": rate, "mean_prec": mean_prec}
+    return all_keys
 
 def judge(keys, hit):
     if set(keys) & set(hit):
@@ -59,6 +62,7 @@ def plot_result(df):
     ax1.set_xlabel("alpha", fontsize=20)
     ax1.set_ylabel("正解のキーワードを抽出できた記事数", fontsize=20)
     ax1.set_yticks([0,20,40,60,80,100,120,140,160,180,200])
+    ax1.set_ylim(0,200)
     ax2.set_ylabel('適合率の平均', fontsize=20)
     ax2.set_yticks([round(0.10 * x,1) for x in range(11)])
     ax1.xaxis.label.set_color('black')
@@ -103,17 +107,38 @@ if __name__ == "__main__":
             print(a.title)
             print(ext.page(a.infobox))
 
-
+    all_key = [[0.0,0],[0.1,0],[0.2,0],[0.3,0],[0.4,0],[0.5,0],[0.6,0],[0.7,0],[0.8,0],[0.9,0],[1.0,0]]
     for key, value in articles.items():
         print("########## Infobox: {}".format(key))
         for alpha in [round(0.10 * x,1) for x in range(11)]:
             result = evaluate(value, tfidf_d, alpha, hits[key])
+            for l in all_key:
+                if l[0] == alpha:
+                    l[1] += result
             results[key].append(result)
             print(result)
 
-        plot_result(pd.DataFrame(results[key]))
+        # plot_result(pd.DataFrame(results[key]))
 
+    r = []
+    for value in all_key:
+        r.append({"alpha":value[0], "keyword_num":value[1]/1000})
+    r_df = pd.DataFrame(r)
 
+    print(r_df)
+
+    ax1 = r_df.plot(kind="bar",color=['black'], edgecolor='black', x="alpha", y=["keyword_num"], linewidth=2)
+
+    h1, l1 = ax1.get_legend_handles_labels()
+
+    ax1.legend(h1, ["選出される特徴語数の平均"], loc='lower left', bbox_to_anchor=(0, 1.01))
+    ax1.set_xlabel("alpha", fontsize=20)
+    ax1.set_ylabel("選出される特徴語数の平均", fontsize=20)
+    ax1.xaxis.label.set_color('black')
+    ax1.yaxis.label.set_color('black')
+    ax1.yaxis.grid(True, which = 'major', linestyle = '-', color = '#CFCFCF')
+    ax1.set_axisbelow(True)
+    plt.show()
 
     raw.close()
     tfidf_raw.close()
