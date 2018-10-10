f12399aaf23a19ad4bc97c84ae0a0e205975e7fb
Hiromu Ishikawa <rock.lee.012086@gmail.com>
2018-10-10 23:41:11 +0900

    Get sections

--- extract_similar.py
+++ extract_similar.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from backend.models.template import Template
+from backend.models.page import Article
 import pandas as pd
 import numpy as np
 import sys
@@ -8,7 +9,12 @@ if __name__ == "__main__":
 
     params = sys.argv
     keys = params[1].split(",")
+    Article.read_tfidf()
+    print("complete reading tfidf file.")
     template = Template({"title":"test_article", "keys":keys})
 
     template.select_similar()
-    template.reccomended_infobox()
+    template.recommended_infobox()
+    print("Infobox: '{}'".format(template.infobox[0].title))
+    template.recommended_sections()
+    print("Section: {}".format(template.secs))
