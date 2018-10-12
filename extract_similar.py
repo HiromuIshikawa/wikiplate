ac3782597b57b8fad775b774b8b140023b13d669
Hiromu Ishikawa <rock.lee.012086@gmail.com>
2018-10-12 16:09:04 +0900

    Generate template by stdin input

--- extract_similar.py
+++ extract_similar.py
@@ -8,13 +8,18 @@ import sys
 if __name__ == "__main__":
 
     params = sys.argv
-    keys = params[1].split(",")
     Article.read_tfidf()
-    print("complete reading tfidf file.")
-    template = Template({"title":"test_article", "keys":keys})
+    while 1:
+        line = input("input keywords: ")
+        if line == "quit":
+            print("bye...")
+            break
+        else:
+            keys = line.split(",")
+            template = Template({"title":"test_article", "keys":keys})
 
-    template.select_similar()
-    template.recommended_infobox()
-    print("Infobox: '{}'".format(template.infobox[0].title))
-    template.recommended_sections()
-    print("Section: {}".format(template.secs))
+            if template.select_similar():
+                template.recommended_infobox()
+                print("Infobox: '{}'".format(template.infobox[0].title))
+                template.recommended_sections()
+                print("Section: {}".format(template.secs))
