fa31d292bf6ee719847b16cc9e0ee156d07609a9
Hiromu Ishikawa <rock.lee.012086@gmail.com>
2018-11-08 23:06:09 +0900

    Fix style of SimilarsList page

--- frontend/src/components/SimilarsList.vue
+++ frontend/src/components/SimilarsList.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="similars">
     <h1>類似記事</h1>
-    <div v-for="similar in sharedSimilars" :key="similar.title"><a :href="'https://ja.wikipedia.org/wiki/' + similar.title" target="_blank">{{similar.title}}</a></div>
+    <b-table id="similars-table" :items="sharedSimilars" :fields="fields" :per-page="perPage" :current-page="currentPage">
+      <template slot="title" slot-scope="data">
+        <a :href="'https://ja.wikipedia.org/wiki/' + data.value" target="_blank">{{data.value}}</a>
+      </template>
+      <template slot="sections" slot-scope="data">
+        {{data.value.join(', ')}}
+      </template>
+      <template slot="table-caption">
+        {{ sharedSimilars.length }} 件の類似記事が抽出されました．
+      </template>
+    </b-table>
+    <b-pagination align="center" size="sm" :total-rows="sharedSimilars.length" :per-page="10" v-model="currentPage">
+    </b-pagination>
   </div>
 </template>
 
@@ -11,7 +23,14 @@ export default {
   name: 'SimilarsList',
   data () {
     return {
-      sharedSimilars: store.state.similars
+      sharedSimilars: store.state.similars,
+      currentPage: 1,
+      perPage: 10,
+      fields: [
+        {key: 'title', label: '記事名', sortable: true},
+        {key: 'infobox', label: 'Infobox', sortable: true},
+        {key: 'sections', label: '章構成', sortable: true}
+      ]
     }
   }
 }
@@ -19,4 +38,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .similars {
+    height: 100%;
+  }
+  #similars-table {
+    font-size: 15px;
+  }
 </style>
