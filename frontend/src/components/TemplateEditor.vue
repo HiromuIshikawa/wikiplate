3d7df69080da934d5dd162fde09556c9adf36f6a
Hiromu Ishikawa <rock.lee.012086@gmail.com>
2018-11-08 23:05:35 +0900

    Fix style of TemplateEditor page

--- frontend/src/components/TemplateEditor.vue
+++ frontend/src/components/TemplateEditor.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="template">
     <h1>記事テンプレート</h1>
-    <p><b>記事名:</b> {{ sharedTemplate.title }}</p>
-    <p><b>Infobox:</b> <a :href="url" target="_blank">{{ sharedTemplate.infobox }}</a></p>
-    <p><b>章構成:</b> {{ sharedTemplate.sections }}</p>
-    <p><b>wikiテンプレート:</b></p>
-    <textarea v-model="sharedTemplate.wiki" name="wiki" cols="60"></textarea>
+    <b-row class="template-row">
+      <b-col class="template-info" cols="12" sm="6">
+        <p><b>記事名:</b> {{ sharedTemplate.title }}</p>
+        <p><b>Infobox:</b> <a :href="url" target="_blank">{{ sharedTemplate.infobox }}</a></p>
+        <p><b>章構成:</b></p>
+        <div class="section-table">
+          <div v-for="(section, index) in sharedTemplate.sections" :key="section">{{index+1}}. {{section}}</div>
+        </div>
+      </b-col>
+      <b-col class="wiki-template" cols="12" sm="6">
+        <p><b>wikiテンプレート:</b></p>
+        <textarea v-model="sharedTemplate.wiki" name="wiki"></textarea>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -28,11 +37,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .template {
+  .template, .template-info, .wiki-template{
     height: 100%;
+  }
+  .template-row {
+    height: 90%;
+  }
+  .section-table {
+    margin-right: 30%;
+    padding: 0.5em 1em;
+    border: solid 2px #888;
+    border-radius: 10px;
   }
   textarea {
     width: 100%;
-    height: 65%;
+    height: 90%;
+    border: solid 2px #888;
   }
 </style>
